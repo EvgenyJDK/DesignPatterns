dce731bc2d32823186b15b0dffd84d58abf1f049
Administrator <evgenyjdk@gmail.com>
2017-06-20 15:40:30 -0700

    add Facade to LibraryAPI: private properties + init + methods

--- DesignPatterns/LibraryAPI.swift
+++ DesignPatterns/LibraryAPI.swift
@@ -11,7 +11,8 @@ import Foundation
 
 
 class LibraryAPI: NSObject {
-
+    
+    
     //1
     class var sharedInstance: LibraryAPI {
         //2
@@ -22,6 +23,44 @@ class LibraryAPI: NSObject {
         //4
         return Singleton.instance
     }
+
+   
+    private let persistencyManager: PersistencyManager
+    private let httpClient: HTTPClient
+    private let isOnline: Bool
+    
+    
+    override init() {
+        persistencyManager = PersistencyManager()
+        httpClient = HTTPClient()
+        isOnline = false
+        
+        super.init()
+    }
+    
+    
+    
+    
+    func getAlbums() -> [Album] {
+        return persistencyManager.getAlbums()
+    }
+    
+    func addAlbum(album: Album, index: Int) {
+        persistencyManager.addAlbum(album, index: index)
+        if isOnline {
+            httpClient.postRequest("/api/addAlbum", body: album.description)
+        }
+    }
+    
+    func deleteAlbum(index: Int) {
+        persistencyManager.deleteAlbumAtIndex(index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
+    
+    
+
     
     
     
