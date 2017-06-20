4784807c5cbb869c5102013181be5799f3608ff1
Administrator <evgenyjdk@gmail.com>
2017-06-20 12:48:35 -0700

    initial commit

---
+++ DesignPatterns/HTTPClient.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPClient.swift
+//  DesignPatterns
+//
+//  Created by Administrator on 20.06.17.
+//  Copyright © 2017 Administrator. All rights reserved.
+//
+
+import Foundation
+
+class HTTPClient {
+    
+    
+    
+}
